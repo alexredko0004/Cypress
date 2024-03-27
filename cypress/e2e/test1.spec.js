90998d7854356c4f501dfacff5d560d6ed8f4d9d
alexredko0004 <alexredko0004@gmail.com>
2024-03-27 23:32:13 +0200

    uncommented steps

--- cypress/e2e/test1.spec.js
+++ cypress/e2e/test1.spec.js
@@ -1,21 +1,21 @@
 describe('test1', () => {
     it('first one', () => {
         cy.visit('/checkbox');
-        // cy.on('uncaught:exception', (err, runnable) => {
-        //     return false
-        //   })
-        // cy.contains('Forms').click();
-        // cy.get('.show .text').click();
-        // cy.get('#firstName').click();
-        // cy.get('#firstName').type('aaa');
-        // cy.get('#lastName').click();
-        // cy.get('#lastName').type('bbb');
-        // cy.get('.custom-radio:nth-child(1) > .custom-control-label').click();
-        // cy.get('#gender-radio-1').click();
-        // cy.get('#userNumber').click();
-        // cy.get('#userNumber').type('3532452352');
-        // cy.get('#submit').click();
-        // cy.get('#userForm').submit();
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
+          })
+        cy.contains('Forms').click();
+        cy.get('.show .text').click();
+        cy.get('#firstName').click();
+        cy.get('#firstName').type('aaa');
+        cy.get('#lastName').click();
+        cy.get('#lastName').type('bbb');
+        cy.get('.custom-radio:nth-child(1) > .custom-control-label').click();
+        cy.get('#gender-radio-1').click();
+        cy.get('#userNumber').click();
+        cy.get('#userNumber').type('3532452352');
+        cy.get('#submit').click();
+        cy.get('#userForm').submit();
         
   
     })
