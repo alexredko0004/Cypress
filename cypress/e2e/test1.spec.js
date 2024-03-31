24b38b8ac003e9bcdeb3ee7155dbdc87c71706b1
alexredko0004 <alexredko0004@gmail.com>
2024-03-31 21:09:57 +0300

    Added login command and ModelsPage pom

--- cypress/e2e/test1.spec.js
+++ cypress/e2e/test1.spec.js
@@ -10,8 +10,6 @@ describe('Tests using POM', () => {
         ModelsPage.createNewModel('Test1','AAA');
         cy.url().should('contains', 'aaa');
         cy.get('h1').should('contain.text', 'Test1');
-        ModelsPage.openModelsPage();
-        cy.contains('Your Work').click()
     })
     //cleaning up all created models
     after(()=>{
